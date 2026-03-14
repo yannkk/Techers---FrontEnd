100478f1006bcb11a594688df5a2f9d2d6d1257f
Adryan Thesse de Oliveira <adryan.alcantara@techers.local>
2026-03-14 10:04:31 -0300

    Aula de JavaScript e Cards

---
+++ aula4/script.js
@@ -0,0 +1,10 @@
+document.getElementsById()
+document.getElementsByClassname()
+document.getElementsByTagName()
+
+//Para alterar conteudo
+titulo.textContext = "Titulo legalzao em JS"
+
+titulo.style.color = "purple"
+
+
---
+++ aula4/script1.js
@@ -0,0 +1,11 @@
+let src = "imagemlalalala.jpg"
+let text = 'descrição'
+let titulo = 'Titulo'
+
+const Infocard = document.getElementById("Info1")
+const tituloCard = document.getElementById('titulo1')
+const imgCard = document.getElementById('img1')
+
+imgCard.setAttribute ('src', src)
+Infocard.textcontent = text
+tituloCard.textcontent = titulo
