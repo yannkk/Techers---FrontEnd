f64329a916a5095adf62d025560c55dd648a150d
Adryan Thesse de Oliveira <adryan.alcantara@techers.local>
2026-04-18 09:59:33 -0300

    Projeto de Notas

---
+++ aula9/script.js
@@ -0,0 +1,8 @@
+let tituloTarefa = document.getElementById("tituloTarefa")
+let conteudoTarefa = document.getElementById("conteudoTarefa")
+let buttonTarefa = document.getElementById("buttonTarefa")
+let notesTarefa = document.getElementById("notesTarefa")
+
+buttonTarefa.addEventListener("click", function(event) {
+
+})
